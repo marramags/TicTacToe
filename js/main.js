a5e67e4dc19b4616e0221774672e2bcaaa5df1b4
marramags <marra.emata@gmail.com>
2022-07-30 11:41:41 -0700

    update-two

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert ("help!!!!!!!");
+// alert ("help!!!!!!!");
 
 /*----- constants -----*/
 // null is when the square is empty
@@ -9,35 +9,51 @@ alert ("help!!!!!!!");
     'player2' : 'purple',
  }
 
- const player1 = 'x';
- const player2 = 'o';
+//  const player1 = 'x';
+//  const player2 = 'o';
 
 //  8 possible winning combinations - three indexes of the board
-const winningCombo = {
-    'horizontalTopRow' : [1, 2, 3],
-    'horizontalMidRow' : [4, 5, 6],
-    'horizontalBotRow' : [7, 8, 9],
-    'verticalTopRow' : [1, 4, 7],
-    'verticalMidRow' : [2, 5, 8],
-    'verticalBotRow' : [3, 6, 9],
-    'diagonalLeft' : [1, 5, 9],
-    'diagonalRight' : [3, 5, 7],
-}
+// const winningCombo = {
+//     'horizontalTopRow' : [1, 2, 3],
+//     'horizontalMidRow' : [4, 5, 6],
+//     'horizontalBotRow' : [7, 8, 9],
+//     'verticalTopRow' : [1, 4, 7],
+//     'verticalMidRow' : [2, 5, 8],
+//     'verticalBotRow' : [3, 6, 9],
+//     'diagonalLeft' : [1, 5, 9],
+//     'diagonalRight' : [3, 5, 7],
+// }
 
-//  board array represents the squares
-const boardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const winningCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+   
+
+
+
 
 /*----- app's state (variables) -----*/
+//  board array represents the squares
+let boardArray = [];
+
 // state variables: 
 let turn;
-let winner = {
-    'won' : ,
-    'tie' : ,
-    'stillPlaying' : ,
+let winner = 
+{
+    'won' : '',
+    'tie' : '',
+    'gameInPlay' : '',
 };
 
 /*----- cached element references -----*/
-let square1 = document.getElementById('1');
+// let squares = document.querySelectorAll('.squares');
 
 
 
